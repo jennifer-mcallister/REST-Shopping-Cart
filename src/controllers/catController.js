c187a3aefc4c3404a09099882c4e03b67269a0af
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-19 19:25:46 +0100

    getCatById should work properly

--- src/controllers/catController.js
+++ src/controllers/catController.js
@@ -1,13 +1,19 @@
 const Cat = require('../models/Cat');
+const { BadRequestError, NotFoundError } = require('../utils/error')
 
 exports.getCatById = async (req, res, next) => {
-    return res.send("cat");
+    console.log("trying to get cat by id");
+    const catId = req.params.id;
+    const cat = await Cat.findById(catId);
+    if(!cat) throw new NotFoundError('That cat does not exist in our store');
+    console.log(cat);
+    return res.json(cat);
 }
 
 exports.getAllCats = async (req, res, next) => {
+    console.log("get all my cats");
+    const catId = req.params.id;
+    console.log(catId)
     const cats = await Cat.find();
-    console.log("trying to get cats");
-    console.log(cats);
     return res.json(cats);
-
 }
--- src/routes/catRoutes.js
+++ src/routes/catRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAllCats
+    getAllCats, 
+    getCatById,
 } = require('../controllers/catController');
 
 router.get("/", getAllCats);
+router.get('/:id', getCatById);
 
 module.exports = router;
